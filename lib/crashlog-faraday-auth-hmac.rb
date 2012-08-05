98a525593eff93b9839dd12cf3810dbdbbee1531
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-08-05 19:29:55 +1000

    Fixed basic requires

--- lib/crashlog-faraday-auth-hmac.rb
+++ lib/crashlog-faraday-auth-hmac.rb
@@ -1,11 +1,1 @@
-require "crashlog-faraday-auth-hmac/version"
-
-module Crashlog
-  module Faraday
-    module Auth
-      module Hmac
-        # Your code goes here...
-      end
-    end
-  end
-end
+require "crash_log/faraday/auth_hmac"
--- lib/crashlog-faraday-auth-hmac/version.rb
+++
@@ -1,9 +0,0 @@
-module Crashlog
-  module Faraday
-    module Auth
-      module Hmac
-        VERSION = "0.0.1"
-      end
-    end
-  end
-end
